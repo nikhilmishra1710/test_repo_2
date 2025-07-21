97f4f80affd71fd038d8f83af874210b3f8bcab1
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2025-07-21 12:52:27 +0530

    Check 5

--- folder2/test.py
+++ folder2/test.py
@@ -3,7 +3,7 @@ import logging
 logging.debug("hello world")
 logging.debug("Byee world")
 
-logging.info("This is a test log")
+logging.info("This is a test log  ")
 
 if "some logging.info" in "some logging.info":
     logging.info("Another test log")
