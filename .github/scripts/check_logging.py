e5d943cd88eae05f0dbd7339fa5a41a0748453cb
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2025-07-23 14:38:09 +0530

    New workflow 12

--- .github/scripts/check_logging.py
+++ .github/scripts/check_logging.py
@@ -44,7 +44,7 @@ def parse_diff_with_line_numbers(filepath: str, diff_range: str) -> List[Tuple[i
 
 def check_logging_info(filepath: str, diff_range: str) -> int:
     count = 0
-    output = ""
+    output = []
     try:
         added_lines = parse_diff_with_line_numbers(filepath, diff_range)
         for lineno, line in added_lines:
@@ -52,7 +52,7 @@ def check_logging_info(filepath: str, diff_range: str) -> int:
             if line.startswith("logging.info(") and not line.endswith("#--- IGNORE ---"):
                 print(f"{filepath}:{lineno}: {line}")
                 print(f"::error file={filepath},line={lineno}::Avoid using logging.info in production code.")
-                output += f"{filepath}:{lineno}: {line}"
+                output.append({"filepath": filepath,"lineno":lineno, "line": line})
                 count += 1
     except subprocess.CalledProcessError as e:
         print(f"Failed to parse diff for {filepath}: {e}")
@@ -110,12 +110,6 @@ def main():
     diff_range = os.environ.get("DIFF_RANGE", "HEAD^..HEAD")
     changed_files = get_changed_files(diff_range)
     
-    if not changed_files:
-        print("No Python files changed in the specified diff range.")
-        print("::set-output name=logging_info_violations_details::[]")
-        print("::set-output name=failed::false")
-        print("::set-output name=logging_info_violations_count::0")
-        return
     total_violations = 0
     violations = []
     for file in changed_files:
@@ -123,19 +117,7 @@ def main():
             count, output = check_logging_info(file, diff_range)
             if count > 0:
                 total_violations += count
-                op = output.split(':')
-                violations.append({"file": op[0], "line": op[1], "details": op[2].strip()})
-
-    if total_violations == 0:
-        print("No `logging.info` violations found.")
-        print("::set-output name=logging_info_violations_details::[]")
-        print("::set-output name=failed::false")
-        print("::set-output name=logging_info_violations_count::0")
-        return
-    
-    print(f"::set-output name=logging_info_violations_details::{json.dumps(violations)}")
-    print(f"::set-output name=failed::true")
-    print(f"::set-output name=logging_info_violations_count::{len(violations)}")
+                violations.extend(output)
     
     post_sticky_comment(violations, total_violations)
 
