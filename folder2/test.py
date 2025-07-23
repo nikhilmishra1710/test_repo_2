ee4c970999991ef864c4e5cdcb56d6309d4340b6
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2025-07-23 13:06:04 +0530

    Test commit 4

--- folder2/test.py
+++ folder2/test.py
@@ -6,4 +6,8 @@ logging.debug("Byee world")
 logging.info("This is a test log  ")
 
 if "some logging.info" in "some logging.info":
-    logging.info("Another test log")
+    logging.info("Another test og")
+    logging.info("Yet another test log")
+    
+else:
+    logging.info("This is not a test log")
