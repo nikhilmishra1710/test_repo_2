764609705e5ca87bf913bb47b6639c8b22a99a94
nikhilMishra-bst <nikhil.mishra@bluestacks.com>
2025-09-17 15:31:26 +0530

    commit 2

--- test3.py
+++ test3.py
@@ -3,7 +3,7 @@ import logging
 logging.debug("hel lo world")
 logging.debug("Byee world logging.debug('Byee world')")
 
-logging.info("This is a test log")
+logging.info("This is a    test log")
 
 
 """
